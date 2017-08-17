6b7d99d257bf8242d11c9faa362972c058423847
soheil <sadabadi@college.harvard.edu>
2017-08-17 10:52:24 +0430

    Mainactivity java done, added new activities

---
+++ app/src/main/java/com/touchteach/touchteach/tools/Users.java
@@ -0,0 +1,20 @@
+package com.touchteach.touchteach.tools;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Soheil on 8/17/2017.
+ */
+
+public class Users {
+    public String email,fname,lname,id;
+    public String[] subjects,relations, classes;
+    public BackendlessUser user;
+    public Users(){
+
+    }
+}
